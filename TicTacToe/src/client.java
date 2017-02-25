899c6296c3e0e7f212d12e606d6ce06eb1b9b1e8
phoenix2114 <hendersona6@nku.edu>
2017-02-25 15:27:02 -0500

    trying to make it work

---
+++ TicTacToe/src/client.java
@@ -0,0 +1,38 @@
+//Always plays 0
+
+import java.net.*;
+import java.io.*;
+
+
+public class client
+{
+
+
+	public static void main( String[] args)
+	{		
+		
+	  Socket toserversocket;
+	  int   reply;   // for later use
+		
+	  System.out.println("CLIENT is attempting connection....");
+	  try {
+		toserversocket = new Socket("localhost", 7788);
+		System.out.println("CONNECTION HAS BEEN MADE");
+		
+		  DataInputStream in = new DataInputStream(toserversocket.getInputStream());
+          DataOutputStream out = new DataOutputStream(toserversocket.getOutputStream());
+
+          //AT THIS POINT CONNECTION MADE
+
+          System.out.println("RECEIVED REQUEST");
+          out.writeDouble(55.1);
+          out.writeDouble(44.7);
+
+          Double answer = in.readDouble();
+	  }
+        catch (IOException e) {
+        	System.out.println(e.toString());
+            System.out.println("It Didn't work - client");
+        }
+    }  // end main
+}  // end myserver class
